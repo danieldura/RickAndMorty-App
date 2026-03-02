909d77fbec2f9616e2be7a65b21e8b8bd801abde
Dani Durà <danieldura7@gmail.com>
2026-03-02 11:39:38 +0100

    chore(config): Set up project

---
+++ RickAndMorty-App/RickAndMorty_App.swift
@@ -0,0 +1,19 @@
+//
+//  RickAndMorty_App.swift
+//  RickAndMorty-App
+//
+//  Created by Daniel Dura on 2/3/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RickAndMortyAppApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+
+        }
+    }
+}
